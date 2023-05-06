a26935294238bd794861e7eec025cc5288b2472e
bissakov <78418192+bissakov@users.noreply.github.com>
2023-05-07 00:02:56 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
+from sqlalchemy.engine.base import Engine
 from dataclasses import dataclass, fields
 from typing import Optional, List, Tuple
 import rich
@@ -22,7 +23,7 @@ class Config:
 
 @dataclass
 class QueryResult:
-    rows: List
+    rows: List[Tuple]
     columns: Tuple
 
 
@@ -32,33 +33,30 @@ class ConnectionNotEstablishedError(Exception):
 
 
 class WrongConfigError(Exception):
-    def __init__(self, error_msg: str) -> None:
-        super().__init__(error_msg)
+    pass
 
 
 class Database:
     def __init__(self, config: Config) -> None:
-        self.config = config
-        self.engine = None
+        self.config: Config = config
+        self.engine: Optional[Engine] = None
 
-    def connect(self):
+    def connect(self) -> None:
         if self.config.dbtype == 'sqlite':
-            self.engine = create_engine('sqlite:///' + self.config.dbname)
+            self.engine = create_engine(f'sqlite:///{self.config.dbname}')
+        elif self.config.credentials is None or None in (self.config.host, self.config.port):
+            error_msg = ', '.join(field.name for field in fields(self.config) if not getattr(self.config, field.name))
+            raise WrongConfigError(f'Config.{error_msg} not provided.')
         else:
-            if self.config.credentials is None or self.config.host is None or self.config.port is None:
-                error_msg = ', '.join(field.name for field in fields(self.config) if not getattr(self.config, field.name))
-                raise WrongConfigError(f'Config.{error_msg} not provided.')
+            connection_string = f'{self.config.dbtype}+pymysql' if self.config.dbtype == 'mysql' else self.config.dbtype
+            user, password = self.config.credentials.user, self.config.credentials.password
+            host, port, dbname = self.config.host, self.config.port, self.config.dbname
+            self.engine = create_engine(f'{connection_string}://{user}:{password}@{host}:{port}/{dbname}')
+        rich.print(self.engine, type(self.engine))
 
-            if self.config.dbtype == 'postgresql':
-                self.engine = create_engine(f'postgresql://{self.config.credentials.user}:{self.config.credentials.password}@' \
-                                            f'{self.config.host}:{self.config.port}/{self.config.dbname}')
-            elif self.config.dbtype == 'mysql':
-                self.engine = create_engine(f'mysql+pymysql://{self.config.credentials.user}:{self.config.credentials.password}@' \
-                                        f'{self.config.host}:{self.config.port}/{self.config.dbname}')
-    
     def execute(self, sql: str) -> QueryResult:
         if self.engine is None:
-            raise ConnectionNotEstablishedError()
+            raise ConnectionNotEstablishedError() 
 
         with self.engine.connect() as conn:
             result = conn.execute(text(sql))
@@ -66,16 +64,16 @@ class Database:
             rows = [tuple(row) for row in result.fetchall()]
         return QueryResult(rows, columns)
     
-    def commit(self):
+    def commit(self) -> None:
         if self.engine is None:
-            raise ConnectionNotEstablishedError()
+            raise ConnectionNotEstablishedError() 
 
         with self.engine.connect() as conn:
             conn.commit()
     
-    def rollback(self):
+    def rollback(self) -> None:
         if self.engine is None:
-            raise ConnectionNotEstablishedError()
+            raise ConnectionNotEstablishedError() 
 
         with self.engine.connect() as conn:
             conn.rollback()
@@ -93,6 +91,7 @@ if __name__ == '__main__':
     #     rich.print(row)
     # rich.print(data)
 
-    config = Config(dbtype='postgresql', dbname='test', host='test', port=5000)
+    config = Config(dbtype='sqlite', dbname='chinook.db')
     db = Database(config)
     db.connect()
+    # db.execute('')
